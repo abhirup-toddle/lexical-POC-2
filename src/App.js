70303a55f86b4334151d08196f451bda70be2510
abhirup-toddle <118720557+abhirup-toddle@users.noreply.github.com>
2023-03-02 11:25:07 +0530

    clean up and add buttons back to toolbar

--- src/App.js
+++ src/App.js
@@ -19,23 +19,8 @@ import Editor from "./Editor";
 export default function App() {
   return (
     <div className="App">
-      <h1>Rich Text Example</h1>
-      <p>Note: this is an experimental build of Lexical</p>
+      <h1>Lexical POC</h1>
       <Editor />
-      <div className="other">
-        <h2>Other Examples</h2>
-        <ul>
-          <li>
-            <a
-              href="https://codesandbox.io/s/lexical-plain-text-example-g932e"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Plain text example
-            </a>
-          </li>
-        </ul>
-      </div>
     </div>
   );
 }
